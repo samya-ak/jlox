ccfde697eceb65a2126c3fe0d2ae0b505f431583
Samyak Maharjan <samyakjyapu33127@gmail.com>
2021-11-03 12:23:19 +0545

    scan identifiers and keywords

--- jlox/Scanner.java
+++ jlox/Scanner.java
@@ -1,16 +1,39 @@
 package jlox;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static jlox.TokenType.*;
 
 public class Scanner {
 	private final String source;
 	private final List<Token> tokens = new ArrayList<>();
+	private static final Map<String, TokenType> keywords;
 	private int start = 0;
 	private int current = 0;
 	private int line = 1;
+
+	static {
+		keywords = new HashMap<>();
+		keywords.put("and", AND);
+		keywords.put("class", CLASS);
+		keywords.put("else", ELSE);
+		keywords.put("false", FALSE);
+		keywords.put("for", FOR);
+		keywords.put("fun", FUN);
+		keywords.put("if", IF);
+		keywords.put("nil", NIL);
+		keywords.put("or", OR);
+		keywords.put("print", PRINT);
+		keywords.put("return", RETURN);
+		keywords.put("super", SUPER);
+		keywords.put("this", THIS);
+		keywords.put("true", TRUE);
+		keywords.put("var", VAR);
+		keywords.put("while", WHILE);
+	}
 
 	Scanner(String source) {
 		this.source = source;
@@ -96,11 +119,33 @@ public class Scanner {
 		default:
 			if (isDigit(c)) {
 				number();
-			} {
-			Lox.error(line, "Unexpected character.");
-		}
+			} else if (isAlpha(c)) {
+				identifier();
+			} else {
+				Lox.error(line, "Unexpected character.");
+			}
 			break;
 		}
+	}
+
+	private void identifier() {
+		while (isAlphaNumeric(peek()))
+			advance();
+
+		String text = source.substring(start, current);
+		TokenType type = keywords.get(text);
+		if (type == null)
+			type = IDENTIFIER;
+
+		addToken(type);
+	}
+
+	private boolean isAlpha(char c) {
+		return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_';
+	}
+
+	private boolean isAlphaNumeric(char c) {
+		return isAlpha(c) || isDigit(c);
 	}
 
 	private boolean isDigit(char c) {
